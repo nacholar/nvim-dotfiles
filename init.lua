fe9e479ce4b33673898ff2f293139193fe1ce03d
nacholar <imlarraguibel@uc.cl>
2023-04-19 00:30:24 -0400

    feat: new mapping added

--- init.lua
+++ init.lua
@@ -221,6 +221,7 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set({ 'i', 'n', 'v'}, '<C-[>', '<Esc>', { silent = true })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
